3d6df9fbac70d54b80616040f9b88171fefb5fda
baekseungyeop <se01163@naver.com>
2021-12-20 18:52:17 +0900

    12월 20일 개인프로젝트 springbootEscapeRoom 기본설정적용

---
+++ src/main/java/com/EscapeRoom/SpringbootEscapeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.EscapeRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEscapeRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEscapeRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EscapeRoom/SpringbootEscapeRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EscapeRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEscapeRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
